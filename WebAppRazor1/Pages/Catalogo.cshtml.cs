5ff4f8288162f4faacab859873a1e42cab369c48
KlausJung011 <nicolas.nsavs@gmail.com>
2025-08-14 21:07:21 -0400

    Primera modificación del proyecto

---
+++ WebAppRazor1/Pages/Catalogo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppRazor1.Pages
+{
+    public class CatalogoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebAppRazor1/Pages/Contacto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppRazor1.Pages
+{
+    public class ContactoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
