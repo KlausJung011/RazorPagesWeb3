b58b6b768b7ff7fd14cfd17702d12caec72667e2
KlausJung011 <nicolas.nsavs@gmail.com>
2025-08-21 16:05:08 -0400

    Json muestra tares en index

--- WebAppRazor1/Pages/Index.cshtml.cs
+++ WebAppRazor1/Pages/Index.cshtml.cs
@@ -1,10 +1,19 @@
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebAppRazor1.Models;
 
 namespace WebAppRazor1.Pages
 {
     public class IndexModel : PageModel
     {
+        public List<Tarea> Tareas { get; set; }
+        public int PaginaActual { get; set; }
+        public int TotalPaginas { get; set; }
+        public int TamanoPagina { get; set; } = 5;//Cambiar de manera dinámica este número en un pequeño input
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -12,9 +21,21 @@ namespace WebAppRazor1.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public void OnGet(int pagina = 1)
         {
+            // Ruta al archivo JSON (asegúrate de que exista en tu proyecto)
+            string jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "tareas.json");
 
+            // Leer el JSON y deserializarlo
+            var jsonContent = System.IO.File.ReadAllText(jsonFilePath);
+            var todasLasTareas = JsonSerializer.Deserialize<List<Tarea>>(jsonContent);
+
+            // Agregar un filtro
+
+            // Lógica de paginación
+            PaginaActual = pagina < 1 ? 1 : pagina;
+            TotalPaginas = (int)Math.Ceiling(todasLasTareas.Count / (double)TamanoPagina);
+            Tareas = todasLasTareas.Skip((PaginaActual - 1) * TamanoPagina).Take(TamanoPagina).ToList();
         }
     }
 }
